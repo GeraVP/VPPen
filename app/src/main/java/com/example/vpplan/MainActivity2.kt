84532915b0d980b8b8d33c4d779b7a2f3e3e1d8f
GeraVP <vpgtr@mail.ru>
2023-12-29 13:27:39 +0300

    29.12.23

--- app/src/main/java/com/example/vpplan/MainActivity2.kt
+++ app/src/main/java/com/example/vpplan/MainActivity2.kt
@@ -2,10 +2,27 @@ package com.example.vpplan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.example.vpplan.R.id.mainImageLay
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+    }
+    fun onClickAddImage(view: View) {
+
+        val yourElement = findViewById<View>(R.id.mainImageLay)
+        if (yourElement.visibility !== view.visibility) {
+            yourElement.visibility = View.VISIBLE
+        } else
+        {
+            yourElement.visibility = View.GONE
+            Toast.makeText(this, "Закрыл", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
